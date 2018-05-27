254849f8517c5975fe03f3700453636e7a2619c2
Philipp Fock <philipp.fock18@gmail.com>
2018-05-27 16:12:46 +0200

    setup with jest & appcomponent test to jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import { AppComponent } from './app.component';
-
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
@@ -19,15 +18,15 @@ describe('AppComponent', function () {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
+    de = fixture.debugElement;
   });
 
   it('should create component', () => expect(comp).toBeDefined() );
 
   it('should have expected <h1> text', () => {
     fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
+    const h1 = de.nativeElement.querySelector("h1");
+    console.log(h1);
+    expect(h1.textContent).toBe("Hello Angular");
   });
 });
